15dc23f8d7d4e5580ecf1475bfc78081d3089739
ossidion <alexinbristol@outlook.com>
2025-07-05 20:13:25 +0100

    add-error-handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,29 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    # @import = User::Import.new
+    @import = User::Import.new
 
     respond_to do |format|
       format.html
-      format.csv { send_data @users.to_csv }
+      format.csv { send_data @users.to_csv, filename: "users-#{Date.today}" }
     end
   end
 
   def import 
-    count = User.import params[:file]
-    redirect_to users_path, notice: "Imported #{count} users"
+    @import = User::Import.new user_import_params
+    if @import.save
+      redirect_to users_path, notice: "Imported #{@import.imported_count} users"
+    else
+      @users = User.all
+      render action: :index, notice: "There were errors with your csv file"
+    # count = User.import params[:file]
+    # redirect_to users_path, notice: "Imported #{count} users"
   end
+end
+
+  
+
+    def user_import_params
+      params.require(:user_import).permit(:file)
+    end
 end
---
+++ app/models/user/import.rb
@@ -0,0 +1,13 @@
+class User::Import
+  include ActiveModel::Model
+  attr_accessor :file, :imported_count
+
+  def process!
+    errors.add(:base, "ERROR!")
+  end
+
+  def save
+    process!
+    errors.none?
+  end
+end
